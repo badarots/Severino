bf3594cf66cc533669fb08fbd4bdfa444a009fc9
badarot <badarots@gmail.com>
2018-12-16 17:59:20 -0200

    Autenticação com BLAKE2s

--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,33 @@
 #include <Arduino.h>
-#include <InterruptMonitor.h>
 
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
 
-// Definição de pinos
+#include <BLAKE2s.h>
+#include <base64.hpp>
+
+// Definições de pinos
 #define BUTTON_PIN 5
 #define OPEN_PIN 14
 #define LED_PIN 12
 
-// definição de parâmetros
+// definições de parâmetros de tempo
 #define T_DESTRAVADO 60000L
 #define T_ABERTO 1000
-#define BUTTON_PRESS 500
+
+// definições para as mensagens
+#define SEP '$'
 
 //---------------------------------------------//
 //            VARIÁVEIS GLOBAIS
 //---------------------------------------------//
 // variáveis do controle da porta
-InterruptMonitor button(BUTTON_PIN, BUTTON_PRESS);
 unsigned long t_destravado, t_aberto;
 bool destravado, aberto;
 
-
 // variáveis do wifi
-const char* ssid = "????";
-const char* pass = "????";
+const char* ssid = "****";
+const char* pass = "****";
 const unsigned long wifi_rcinterval = 1000; // Intervalo entre reconexões
 unsigned long wifi_lastrc;
 WiFiClient wclient;
@@ -33,12 +35,18 @@ WiFiClient wclient;
 //variável do cliente MQTT
 // const char* mqtt_server = "broker.mqttdashboard.com";
 IPAddress mqtt_server(192,168,1,75);
-const char* mqtt_inTopic = "testario/fechadura";   // nome do tópico de publicação
-const char* mqtt_outTopic = "testario/server";  // nome do tópico de inscrição
+const char* mqtt_inTopic = "testarhs/porta";   // nome do tópico de publicação
+const char* mqtt_outTopic = "testarhs/server";  // nome do tópico de inscrição
 const unsigned long mqtt_rcinterval = 3000;     // Intervalo entre reconexões
 unsigned long mqtt_lastrc;
 PubSubClient mqtt_client(wclient);
 
+// Variáveis para autenticação de msgs
+BLAKE2s blake;
+byte sig_key[] = "you-will-never-guess-again";
+byte key_len = sizeof(sig_key) - 1;
+const byte sig_len = 16;
+const byte b64_len = (sig_len + 2) / 3 * 4;
 
 //---------------------------------------------//
 //            FUNÇÕES
@@ -66,8 +74,21 @@ void abre_porta() {
   mqtt_client.publish(mqtt_outTopic, "Porta aberta");
 }
 
-void button_set() {
-  button.raiseFlag();
+void sign(byte* hash, byte* msg, byte msg_len) {
+  blake.reset(sig_key, key_len, sig_len);
+  blake.update(msg, msg_len);
+  blake.finalize(hash, sig_len);
+}
+
+bool check_payload(byte* msg, byte msg_len, byte* sig) {
+  byte test[sig_len];
+  sign(test, msg, msg_len);
+  byte b64[b64_len];
+  encode_base64(test, sig_len, b64);
+  for (int i = 0; i < b64_len; i++) {
+    if (b64[i] != sig[i]) return false;
+  }
+  return true;
 }
 
 void reconnectWifi() {
@@ -76,19 +97,31 @@ void reconnectWifi() {
   Serial.println("...");
   WiFi.begin(ssid, pass);
 
-if (WiFi.waitForConnectResult() != WL_CONNECTED)
-  return;
-Serial.println("WiFi connected");
+  if (WiFi.waitForConnectResult() != WL_CONNECTED)
+    return;
+
+  Serial.println("WiFi connected");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+randomSeed(micros());
 }
 
 bool reconnectMQTT() {
   Serial.println("Connecting to the MQTT broker...");
-  // if (mqtt_client.connect(mqtt_clientname)) {
-    if (mqtt_client.connect("client_kjhadu4")) {
+  String client_id = "clientid_";
+  client_id += String(random(0xffff), HEX);
+  if (mqtt_client.connect(client_id.c_str())) {
     // Once connected, publish an announcement...
     mqtt_client.publish(mqtt_outTopic, "hello world", true);
     // ... and resubscribe
     mqtt_client.subscribe(mqtt_inTopic);
+  } else {
+    Serial.print('falha, rc=');
+    Serial.print(mqtt_client.state());
+    Serial.print(" tentando novamente em ");
+    Serial.print(mqtt_rcinterval/1000);
+    Serial.println(" segundos");
   }
   return mqtt_client.connected();
 }
@@ -96,25 +129,45 @@ bool reconnectMQTT() {
 // callback que lida com as mensagem recebidas
 void mqtt_callback(char* topic, byte* payload, unsigned int length) {
 
-    // Copy the payload to the new char buffer
-    char* msg = (char*)malloc(length + 1);
-    memcpy(msg, (char*)payload, length);
-    msg[length] = '\0';
+  // encontrando o tamanho da mensagem, limitada por SEP
+  byte msg_len = 0;
+  for (; msg_len < length && payload[msg_len] != SEP; msg_len++);
+  // tamanho da assinatura
+  byte sig_size = length - msg_len - 1;
 
-    // // prepara a msg de resposta
-    // char resp[30];
-    // strncpy(resp, msg, sizeof(resp));
-    // resp[sizeof(resp) -1] = '\0';
+  // ignora msg se a assinatura não tiver o tamanho correto
+  if (sig_size != b64_len) {
+    Serial.println("msg. mal formatada");
+    return;
+  }
 
-    Serial.println(msg);
-    char* command = strtok(msg, "=");  // horário de envio da msg
-    char* temp = strtok(NULL, "="); // comando
-    Serial.print("Comando: "); Serial.println(command);
-    Serial.print("temp: "); Serial.println(temp);
+  // guarda msg como string
+  char* msg = (char*)malloc(msg_len + 1);
+  memcpy(msg, (char*)payload, msg_len);
+  msg[msg_len + 1] = '\0';
 
-    if(strcmp(command, "liberar") == 0) {
-      destravar_porta();
-    }
+  // guarda assinatura como lista de bytes
+  byte* sig = (byte*)malloc(sig_size);
+  memcpy(sig, payload + msg_len + 1, sig_size);
+
+  // testa assinatura
+  bool check = check_payload((byte*)msg, msg_len, sig);
+
+  // ignora msg se a assinatura forneceda é incorreta
+  if (!check) {
+    Serial.println("ass. invalida");
+    return;
+  }
+
+  // agora que a msg foi autenticada execute o que foi pedido
+  Serial.println("ass. autenticada");
+
+  char* command = strtok(msg, ":");     // comando
+  long temp = atol(strtok(NULL, "\0")); // timestamp do envio da msg
+
+  if(strcmp(command, "liberar") == 0 && temp > 0) {
+    destravar_porta();
+  }
 }
 
 //---------------------------------------------//
@@ -128,12 +181,10 @@ void setup() {
   //configuração dos pinos
   pinMode(OPEN_PIN, OUTPUT);
   pinMode(LED_PIN, OUTPUT);
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
 
   // digitalWrite(OPEN_PIN, HIGH);
   // digitalWrite(LED_PIN, HIGH);
-
-  // configura interruptor dos pinos de entrada
-  attachInterrupt(digitalPinToInterrupt(button.pin), button_set, LOW);
 
   // configuracao do wifi
   WiFi.mode(WIFI_STA);
